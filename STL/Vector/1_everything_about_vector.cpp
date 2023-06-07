08311484f810623a5ac7733bc21983d56a7b9121
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-06-08 00:36:12 +0600

    Create 1_everything_about_vector.cpp

---
+++ STL/Vector/1_everything_about_vector.cpp
@@ -0,0 +1,21 @@
+/*
+||-----------------------||
+||  Asif Mohammed Sifat  ||
+||  Department of CSE    ||
+||   CS Instructor       ||
+||Phitron,ProgrammingHero||
+||-----------------------||
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    //===========How to declear vector===============//
+
+    vector<int> vec;  // vector<data_type> vector_name; // Empty vector of integers
+    vector<int> vec(5,0); // vector<data_type> vector_name(size,value) // 0 0 0 0 0  
+    vector<int> num = {1,2,3,4,5}; // Vector of integers initialized with values
+
+              
+    return 0;
+}
