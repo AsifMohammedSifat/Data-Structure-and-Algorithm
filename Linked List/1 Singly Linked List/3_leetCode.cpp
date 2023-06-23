d54fe681754932e16446bf0d25bb751eaade4be7
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-06-23 14:43:08 +0600

    Create 3_leetCode.cpp

---
+++ Linked List/1 Singly Linked List/3_leetCode.cpp
@@ -0,0 +1,10 @@
+1) https://leetcode.com/problems/remove-linked-list-elements/
+2) https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+3) https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+4) https://leetcode.com/problems/merge-nodes-in-between-zeros/
+
+Solution:
+1. https://ideone.com/BNDqHD
+2. https://ideone.com/uI60NM
+3. https://ideone.com/jY1OX2
+4. https://ideone.com/jPHvut
