925c4ea55ecedf5451135cb9b2e3b860d4d4349c
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-06-23 14:48:37 +0600

    Update 3_leetCode.cpp

--- Linked List/1 Singly Linked List/3_leetCode.cpp
+++ Linked List/1 Singly Linked List/3_leetCode.cpp
@@ -1,10 +1,25 @@
-1) https://leetcode.com/problems/remove-linked-list-elements/
-2) https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-3) https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
-4) https://leetcode.com/problems/merge-nodes-in-between-zeros/
+
+//======================================|Level-1|==============================================//
+1) Middle of the Linked List: https://leetcode.com/problems/middle-of-the-linked-list/
+2) Linked List Cycle: https://leetcode.com/problems/linked-list-cycle/
+3) Remove Duplicates from Sorted List: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+4) Reverse Linked List: https://leetcode.com/problems/reverse-linked-list/
+5) Palindrome Linked List: https://leetcode.com/problems/palindrome-linked-list/
+6) Delete Node in a Linked List: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+//=====================================|Level-2|==========================================//
+1) Remove Linked List Elements: https://leetcode.com/problems/remove-linked-list-elements/
+2) Remove Nth Node From End of List: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+3) Swapping Nodes in a Linked List : https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+4) Merge Nodes in Between Zeros : https://leetcode.com/problems/merge-nodes-in-between-zeros/
 
 Solution:
 1. https://ideone.com/BNDqHD
 2. https://ideone.com/uI60NM
 3. https://ideone.com/jY1OX2
 4. https://ideone.com/jPHvut
+//=====================================================================================//
+
+
+
+
