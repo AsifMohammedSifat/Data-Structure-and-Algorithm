bc14c34227f67a2f19cfb9e0cca681f060b4eebf
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-07-19 00:27:08 +0600

    Update 1_Operation.cpp

--- Heap/1_Operation.cpp
+++ Heap/1_Operation.cpp
@@ -8,94 +8,86 @@
 ||Phitron,ProgrammingHero||
 ||-----------------------||
 */
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-class MaxHeap
-{
+class MaxHeap{
 public:
     vector<int> v;
-    MaxHeap()
-    {
+    MaxHeap(){
+
     }
 
-    void up_heapify(int idx)
-    {
-        while (0 < idx && v[idx] > v[(idx - 1) / 2])
-        {
-            swap(v[(idx - 1) / 2], v[idx]);
-            idx = (idx - 1) / 2;
+    void up_heapify(int idx){
+        while(0<idx && v[idx]>v[(idx-1)/2]){
+            swap(v[idx],v[(idx-1)/2]);
+            idx = (idx-1)/2;
         }
-    }
-    void down_heapify(int idx)
-    {
-        while (true)
-        {
-            int largest = idx;
-            int l = 2 * idx + 1;
-            int r = 2 * idx + 2;
 
-            if (l < v.size() && v[largest] < v[l])
-            {
+    }
+
+    //O(logn)
+    void push(int val){
+        v.push_back(val);
+        up_heapify(v.size()-1);        
+    }
+
+    void printHeap(){
+        for(int val:v){
+            cout<<val<<" ";
+        }
+        cout<<'\n';
+    }
+
+    void down_heapify(int idx){
+        while(true){
+            int largest = idx;
+            int l = 2*idx+1;
+            int r = 2*idx+2;
+
+            if(l<v.size() && v[largest]<v[l]){
                 largest = l;
             }
-            if (r < v.size() && v[largest] < v[r])
-            {
+            if(r<v.size() && v[largest]<v[r]){
                 largest = r;
             }
-            if (largest == idx)
-            {
-                break;
-            }
-            swap(v[idx], v[largest]);
+            if(largest==idx) break;
+
+            swap(v[idx],v[largest]);
             idx = largest;
         }
+
     }
 
-    void push(int val)
-    {
-        v.push_back(val);
-        up_heapify(v.size() - 1);
-    }
-
-    void pop()
-    {
-        swap(v[0], v[v.size() - 1]);
+    //O(logn)
+    void pop(int idx){
+        swap(v[idx],v[v.size()-1]);
         v.pop_back();
-        down_heapify(0);
+        down_heapify(idx);
     }
 
-    void printHeap()
-    {
-        for (int val : v)
-        {
-            cout << val << " ";
-        }
-        cout << endl;
-    }
-
-    int top()
-    {
+    //O(1)
+    int getMax(){
         return v[0];
     }
 
-    void buildHeapFromArray(vector<int> arr)
-    {
-        v.clear();
+    //O(n)
+    void buildHeapFromArray(vector<int>&arr){
         v = arr;
-        int n = arr.size();
-        int lastNonLeafNode = n / 2 - 1;
+        int n = v.size();
+        int lastNonLeafNode = n/2-1;
 
-        for (int i = lastNonLeafNode; i >= 0; i--)
-        {
+        for(int i = lastNonLeafNode;i>=0;i--){
             down_heapify(i);
         }
     }
+
+
 };
 
-int main()
-{
+int main(){
     MaxHeap mx;
+
     mx.push(1);
     mx.push(2);
     mx.push(3);
@@ -104,14 +96,14 @@ int main()
 
     mx.printHeap();
 
-    mx.pop();
+    mx.pop(0);
     mx.printHeap();
 
-    cout << mx.top() << endl;
+    cout<<mx.getMax()<<endl;
 
-    vector<int> arr = {1, 2, 3, 4, 5};
+    vector<int> arr = {1,2,3,4,5};
     mx.buildHeapFromArray(arr);
-    mx.printHeap();
-
+    mx.printHeap();         
+              
     return 0;
 }
