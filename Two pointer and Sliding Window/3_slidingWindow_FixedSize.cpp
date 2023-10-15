8cb36880a17c8715922ed00d001d733fc865ade8
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-10-15 21:17:18 +0600

    Update 3_slidingWindow_FixedSize.cpp

--- Two pointer and Sliding Window/3_slidingWindow_FixedSize.cpp
+++ Two pointer and Sliding Window/3_slidingWindow_FixedSize.cpp
@@ -19,6 +19,36 @@ Sample Output:
 3 4 5  = 12
 Maximum subarray summation is:12
 */
+//====================================|Type - 1|=============================================
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int k;
+    cin >> k;
+
+    int i = 0, j = 0, s = 0;
+    int mx = INT_MIN;
+    while (j < n)
+    {
+        s += a[j];
+        if (j >= k - 1)
+        {
+            mx = max(mx, s);
+            s -= a[i];
+            i++;
+        }
+        j++;
+    }
+    cout << mx << endl;
+    return 0;
+}
+//====================================|Type - 2|=============================================
 #include <bits/stdc++.h>
 using namespace std;
 int main()
